24b3f2bcd1cef5099144fa04a9cec93a048ceb88
Komiljon <nortoshovk@gmail.com>
2024-07-17 10:42:56 +0500

    add contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,7 +1,18 @@
 import "./Contacts.scss";
 
 function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <section className="contacts">
+      <div className="container">
+        <p className="contacts__intro">Book Prefered Deal Here</p>
+        <span className="contacts__divider"></span>
+        <h2 className="contacts__title">Make Your Reservation</h2>
+        <a href="#" className="contacts__btn">
+          Discover more
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default Contacts;
